10ffe4eee85af7389b663c337a438503b0b41f53
Fabian Raetz <fabian.raetz@gmail.com>
2013-02-10 13:37:49 +0100

    added initial tests for ContactService

--- ContactManager.Web/Scripts/tests/unit/controllers/navigationCtrlSpecs.js
+++ ContactManager.Web/Scripts/tests/unit/controllers/navigationCtrlSpecs.js
@@ -10,7 +10,7 @@
 describe('NavigationCrtl', function () {
     'use strict';
 
-    var scope, ctrl, $httpBackend;
+    var scope, ctrl;
 
     beforeEach(module('contactmanager'));
 
@@ -19,14 +19,6 @@ describe('NavigationCrtl', function () {
         $controller('NavigationCtrl', { $scope: scope });
     }));
 
-    //beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
-    //    $httpBackend = _$httpBackend_;
-    //    $httpBackend.expectGET('phones/phones.json').
-    //        respond([{ name: 'Nexus S' }, { name: 'Motorola DROID' }]);
-
-    //    scope = $rootScope.$new();
-    //    ctrl = $controller(PhoneListCtrl, { $scope: scope });
-    //}));
 
     it('should attach $location to $scope.location', function () {
         expect(scope.location).not.toBeNull();
---
+++ ContactManager.Web/Scripts/tests/unit/services/contactServiceSpecs.js
@@ -0,0 +1,72 @@
+﻿//reference test dependencies to make http://chutzpah.codeplex.com/ happy  
+
+/// <reference path="\Scripts\angular-1.1.2\angular.js" />
+/// <reference path="\Scripts\angular-1.1.2\angular-mocks.js" />
+
+/// <reference path="\Scripts\app\services\contactService.js" />
+
+describe('ContactsService', function () {
+    'use strict';
+
+    var contactService, $httpBackend;
+
+    beforeEach(module('contactmanager.data'));
+
+    beforeEach(inject(function (ContactService) {
+        contactService = ContactService;
+    }));
+
+    beforeEach(inject(function (_$httpBackend_) {
+
+        //see http://docs.angularjs.org/api/ngMock.$httpBackend
+        $httpBackend = _$httpBackend_;
+    }));
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+
+    it('should get all contacts from backend when GetALLContacts was called', function () {
+        //Arrange
+        var contacts;
+        $httpBackend.expectGET('api/contacts')
+            .respond(200, [{ firstName: 'contact1' }, { firstName: 'contact2' }]);
+
+
+        //Act
+        contactService.getAllContacts().then(function (result) {
+            contacts = result.data;
+        });
+
+        //flush outstanding requests
+        $httpBackend.flush();
+
+        //Assert
+        expect(contacts.length).toBe(2);
+    });
+
+    it('should save contact to backend when saveContact was called', function () {
+        //Arrange
+        var contact = { firstName: 'Mischi' };
+        var expected = { firstName: 'Mischi', id: 'id' };
+        var savedContact;
+        $httpBackend.expectPOST('api/contacts', contact)
+            .respond(201, expected);
+
+        //Act
+        contactService.saveContact(contact).then(function (result) {
+            savedContact = result.data;
+        });
+
+        //flush outstanding requests
+        $httpBackend.flush();
+
+        //Assert
+        expect(savedContact).toEqual(expected);
+    });
+});
+
+
+
